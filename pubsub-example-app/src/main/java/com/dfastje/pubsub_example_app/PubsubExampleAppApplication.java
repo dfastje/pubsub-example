8e49bc52b3df7fc766d11dafed086763ecc4a567
dfastje <dfastjework@protonmail.com>
2024-12-31 22:02:17 -0500

    init commit of java app

---
+++ pubsub-example-app/src/main/java/com/dfastje/pubsub_example_app/PubsubExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.dfastje.pubsub_example_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubExampleAppApplication.class, args);
+	}
+
+}
