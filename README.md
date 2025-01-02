90514c4fa9f0100a6511fde8cd9b91a59e90d89c
dfastje <dfastjework@protonmail.com>
2025-01-01 21:55:04 -0500

    mid-work commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pubsub-example
 A project to make an example app from scratch using RabbitMQ 
+
+
+# Currently working on:
--- RabbitMQ/RabbitMQ.sh
+++ RabbitMQ/RabbitMQ.sh
@@ -1,2 +1,5 @@
-sudo docker compose up -d
-#sudo docker compose down
+#http://localhost:8161/console/auth/login
+#https://activemq.apache.org/components/artemis/documentation/latest/docker.html#official-images
+
+#sudo docker compose up -d
+sudo docker compose down
